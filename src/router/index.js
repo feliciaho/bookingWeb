3be28344fcb5d2645790fb93ed21ed0f43d3660a
unknown <felicia18felicia18@gmail.com>
2025-05-15 12:15:15 +0800

    v1.0

--- src/router/index.js
+++ src/router/index.js
@@ -12,31 +12,27 @@ const router = createRouter({
       name: 'HomeView',
       component: () => import('../views/HomeView.vue'),
     },
-    {
-      path: '/popularView',
-      name: 'PopularView',
-      component: () => import('../views/PopularView.vue'),
-    },
-    {
-      path: '/myFavoriteView',
-      name: 'MyFavoriteView',
-      component: () => import('../views/MyFavoriteView.vue'),
-    },
-    {
-      path: '/recipeView/:id',
-      name: 'RecipeView',
-      component: () => import('../views/RecipeView.vue'),
-      // 將路由參數 id 傳遞為 props
-      props: (route) =>({
-        id: route.params.id
-      })
-    },
+    // {
+    //   path: '/aboutView',
+    //   name: 'AboutView',
+    //   component: () => import('../views/AboutView.vue'),
+    // },
+    // {
+    //   path: '/exploreView',
+    //   name: 'ExploreView',
+    //   component: () => import('../views/ExploreView.vue'),
+    // },
+    // {
+    //   path: '/roomsView',
+    //   name: 'RoomsView',
+    //   component: () => import('../views/RoomsView.vue'),
+    // },
     //新增404page 記得要在router也新增頁面
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'NotFound',
-      component: () => import('../views/NotFoundView.vue'),
-    },
+    // {
+    //   path: '/:pathMatch(.*)*',
+    //   name: 'NotFound',
+    //   component: () => import('../views/NotFoundView.vue'),
+    // },
   ],
   // 每換一次路由就回到最上面
   scrollBehavior() {
--- src/views/NotFoundView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <section>
-    <h1 class="secTitle">404 Error</h1>
-  </section>
-</template>
